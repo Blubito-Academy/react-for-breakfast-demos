8cdf45482ca627e17af53a7bd65218e5c6b6fcf8
LenkoIvanov17 <l.ivanov@blubito.com>
2024-03-17 18:15:03 +0200

    feat: create and style a responsive navbar

---
+++ responsive-design/src/components/Navbar/Navbar.tsx
@@ -0,0 +1,10 @@
+import styles from "./Navbar.module.scss";
+
+export const Navbar = () => {
+    return <ul className={styles.navbar}>
+        <li>Catalog</li>
+        <li>About us</li>
+        <li>Contacts</li>
+        <li className={styles.brandName}>BRANDNAME</li>
+    </ul>
+}
