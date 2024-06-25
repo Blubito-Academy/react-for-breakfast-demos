d6c53e8cd6f5aa918dd36623bc3baa39503e52bf
LenkoIvanov17 <l.ivanov@blubito.com>
2024-06-25 23:03:33 +0300

    feat: create the observable class and the layout

--- observer-pattern/src/App.tsx
+++ observer-pattern/src/App.tsx
@@ -3,8 +3,14 @@ import './App.css'
 function App() {
   
   return (
-    <>
-    </>
+    <div className='observerShowCase'>
+        <h1>Observer pattern</h1>
+        <div className="eventsContainer">
+          <input placeholder='Type something...'></input>
+          <button>Click</button>
+        </div>
+        <code></code>
+    </div>
   )
 }
 
---
+++ observer-pattern/src/ObservableClass.ts
@@ -0,0 +1,21 @@
+class Observable {
+    observers: {(dataToDisplay: string): void} [];
+
+    constructor() {
+        this.observers = [];
+    }
+
+    public subscribe(newObserver: {(dataToDisplay: string): void}) {
+        this.observers.push(newObserver);
+    }
+
+    public unsubscribe(observerToRemove: {(dataToDisplay: string): void}) {
+        this.observers = this.observers.filter((observer) => observer !== observerToRemove);
+    }
+
+    public notifyObservers(dataToDisplay: string) {
+        this.observers.forEach((observer) => observer(dataToDisplay));
+    }
+}
+
+export const observableObject = new Observable();
