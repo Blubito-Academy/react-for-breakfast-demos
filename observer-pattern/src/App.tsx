d60850d4857d734500cb131e631af8f0390bcc0b
LenkoIvanov17 <l.ivanov@blubito.com>
2024-06-25 23:50:28 +0300

    feat: Subscribe logger and toast

--- observer-pattern/src/App.tsx
+++ observer-pattern/src/App.tsx
@@ -1,17 +1,72 @@
-import './App.css'
+import { useEffect, useRef, useState } from "react";
+import { observableObject } from "./ObservableClass";
+import { Toaster, toast } from "react-hot-toast";
+import "./App.css";
 
 function App() {
-  
+  const [changeTriggers, setChangeTriggers] = useState<JSX.Element[]>([]);
+  const useEffectRef = useRef<boolean>(false);
+
+  const triggerAlertObserver = (dataToDisplay: string) => {
+    toast.success(dataToDisplay);
+  };
+
+  const triggerLoggerObserver = (dataToDisplay: string) => {
+    const loggedInfo: JSX.Element = (
+      <div key={dataToDisplay}>{dataToDisplay}</div>
+    );
+    const newChangeTriggers = [...changeTriggers];
+    newChangeTriggers.push(loggedInfo);
+    setChangeTriggers(newChangeTriggers);
+  };
+
+  const handleBtnClick = () => {
+    observableObject.notifyObservers("The button was clicked!");
+  };
+
+  const handleInputChange = (ev: React.ChangeEvent<HTMLInputElement>) => {
+    const logInputChange: string =
+      ev.target.value.length === 0 ? "Emptied input" : ev.target.value;
+    observableObject.notifyObservers(`New input change: ${logInputChange}`);
+  };
+
+  const handleAlertUnsub = () => {
+    observableObject.unsubscribe(triggerAlertObserver);
+  };
+
+  const handleLoggerUnsub = () => {
+    observableObject.unsubscribe(triggerLoggerObserver);
+  };
+
+  useEffect(() => {
+    if (!useEffectRef.current) {
+      observableObject.subscribe(triggerAlertObserver);
+      observableObject.subscribe(triggerLoggerObserver);
+      useEffectRef.current = true;
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
-    <div className='observerShowCase'>
+    <>
+      <div className="observerShowCase">
         <h1>Observer pattern</h1>
         <div className="eventsContainer">
-          <input placeholder='Type something...'></input>
-          <button>Click</button>
+          <input
+            onChange={handleInputChange}
+            placeholder="Type something..."
+          ></input>
+          <button onClick={handleBtnClick}>Click</button>
         </div>
-        <code></code>
-    </div>
-  )
+        <div className="unsubContainer">
+          <button onClick={handleAlertUnsub}>Unsub alert</button>
+          <button onClick={handleLoggerUnsub}>Unsub logger</button>
+        </div>
+        <code>{changeTriggers}</code>
+      </div>
+      <Toaster position="top-right" />
+    </>
+  );
 }
 
-export default App
+export default App;
--- observer-pattern/src/ObservableClass.ts
+++ observer-pattern/src/ObservableClass.ts
@@ -1,21 +1,24 @@
 class Observable {
-    observers: {(dataToDisplay: string): void} [];
+  observers: { (dataToDisplay: string): void }[];
 
-    constructor() {
-        this.observers = [];
-    }
+  constructor() {
+    this.observers = [];
+  }
 
-    public subscribe(newObserver: {(dataToDisplay: string): void}) {
-        this.observers.push(newObserver);
-    }
+  public subscribe(newObserver: { (dataToDisplay: string): void }) {
+    this.observers.push(newObserver);
+  }
 
-    public unsubscribe(observerToRemove: {(dataToDisplay: string): void}) {
-        this.observers = this.observers.filter((observer) => observer !== observerToRemove);
-    }
+  public unsubscribe(observerToRemove: { (dataToDisplay: string): void }) {
+    console.log(observerToRemove);
+    this.observers = this.observers.filter(
+      (observer) => observer !== observerToRemove
+    );
+  }
 
-    public notifyObservers(dataToDisplay: string) {
-        this.observers.forEach((observer) => observer(dataToDisplay));
-    }
+  public notifyObservers(dataToDisplay: string) {
+    this.observers.forEach((observer) => observer(dataToDisplay));
+  }
 }
 
 export const observableObject = new Observable();
